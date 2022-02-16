76eda3eedf78b708b3ecdb696de4327453fb478f
Brian chow <brianchowshunfu@gmail.com>
2022-02-16 21:25:46 +0800

    fix

--- script.js
+++ script.js
@@ -187,12 +187,4 @@ const score = status =>{
   };
 ;}
 
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`
 
